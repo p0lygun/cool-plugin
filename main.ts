6eb76cc703522fac60d0212e5dc31023ea8d6b2a
p0lygun <solankivibhakar82@gmail.com>
2022-10-28 17:11:42 +0530

    feat: use proper classname in rule list

--- main.ts
+++ main.ts
@@ -197,7 +197,7 @@ function populateleftPagePlugins(){
     handelExperienceRulesListing()
 }
 function handelExperienceRulesListing() {
-    const rulesListContaier = $('.collapsedRuleContainer')
+    const rulesListContaier = $('.collapsedRuleInnerContainer')
     if (rulesListContaier.length) {
         listBlocksInModBlock().then(blocks => {
             rulesListContaier.children('.collapsedRule').remove()
