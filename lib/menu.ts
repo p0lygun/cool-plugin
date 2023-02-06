fd015ebd8ccff134d509ed89df0b765cfb9016e9
p0lygun <solankivibhakar82@gmail.com>
2023-02-06 13:29:18 +0530

    feat: add left menu pane and made it draggable

--- lib/menu.ts
+++ lib/menu.ts
@@ -1,5 +1,5 @@
 import { logger, BF2042SDK } from "../main";
-
+import {PlainDraggable } from "../static/js/plain-draggable.min.js";
 export function menuInitlizer() {
   const matTree = $("app-editor-menu mat-tree");
   if (matTree) {
@@ -9,12 +9,29 @@ export function menuInitlizer() {
         const leftMenuButtonImage = $("#cool_leftMenuButtonImage");
         if (leftMenuButtonImage) {
             leftMenuButtonImage.attr("src", BF2042SDK.getUrl("static/images/leftMenuButton.svg"));
-            $('#cool_leftMenuButton').on('click', function () {
-                logger.info("Left Menu Button Clicked");
-            });
+            $('#cool_leftMenuButton').on('click', leftMenuButtonCallback);
         }
     });
   } else {
     logger.critical("Couldn't find root menu tree");
   }
+}
+
+function leftMenuButtonCallback() {
+  logger.info("Left Menu Button Clicked");
+  makeLeftMenuPopup();
+}
+
+export function makeLeftMenuPopup(){
+  $.get(BF2042SDK.getUrl("html/leftMenu/popup.html"), function (data) {
+    $('body').append(data);
+  }).done(function () {
+    const leftMenuPane = $("#leftMenuPane");
+    if(leftMenuPane){
+      const leftMenuPaneDragHandler = new PlainDraggable(leftMenuPane[0]);
+      leftMenuPaneDragHandler.setOptions({
+        handle: leftMenuPane.find('.draggable')[0]});
+    leftMenuPane.show();
+    }
+  })
 }
