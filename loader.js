ad5684284aefa173d5f1f2ac46ee764925591c12
p0lygun <solankivibhakar82@gmail.com>
2023-02-08 22:41:45 +0530

    feat: add timestamp when loading main.js to prevent caching

--- loader.js
+++ loader.js
@@ -1,14 +1,19 @@
-if(!window.cool_plugin_loaded){
-    window.cool_plugin_loaded = false
+if (!window.cool_plugin_loaded) {
+  window.cool_plugin_loaded = false;
 }
-if(!window.exports){
-    exports  = {}
+if (!window.exports) {
+  exports = {};
 }
-if(document.querySelectorAll('[data-cool-plugin]').length == 0){
-    console.log('Loading Cool Plugins')
-    tag = document.createElement('script')
-    tag.setAttribute('type', 'module')
-    tag.setAttribute('data-cool-plugin', 'true')
-    tag.setAttribute('src', BF2042Portal.Plugins.getPlugin('1650e7b6-3676-4858-8c9c-95b9381b7f8c').getUrl('dist/main.js'))
-    document.body.appendChild(tag);
+if (document.querySelectorAll("[data-cool-plugin]").length == 0) {
+  console.log("Loading Cool Plugins");
+  tag = document.createElement("script");
+  tag.setAttribute("type", "module");
+  tag.setAttribute("data-cool-plugin", "true");
+  tag.setAttribute(
+    "src",
+    `${BF2042Portal.Plugins.getPlugin(
+      "1650e7b6-3676-4858-8c9c-95b9381b7f8c"
+    ).getUrl("dist/main.js")}?t=${Date.now()}`
+  );
+  document.body.appendChild(tag);
 }
